9467d97558dc187a96ff5217a3132f80c074d37d
sashka792 <sashka792@gmail.com>
2021-03-28 16:43:21 +0300

    4 module

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ function getRandomIntInclusive (min, max) {
   return Math.abs(randomNum)
 }
 console.log(getRandomIntInclusive(0, 8))
+console.log('4 module')
 
 // Пример взят с Mozilla MDN Web Docs
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
